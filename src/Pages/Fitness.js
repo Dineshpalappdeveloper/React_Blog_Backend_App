083dcc67f11804d1472179c7fc86a1b66193d56f
Dineshpalappdeveloper <dineshkr748199@gmail.com>
2023-10-09 15:06:52 +0530

    new version

--- src/Pages/Fitness.js
+++ src/Pages/Fitness.js
@@ -191,7 +191,7 @@ const Fitness = () => {
 
               </div>
               <div className="advatige advatige-Food theBihar1">
-                <h2 className="advatige-text advatige-text-Food ">advertistment</h2>
+                <h2 className="advatige-text advatige-text-Food bihartext">advertistment</h2>
               </div>
 
             </div>
